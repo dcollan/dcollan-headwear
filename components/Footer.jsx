fc50b15173cea7ca85348929f5a6e63b4c57ffe0
Danny Collan <collan97@gmail.com>
2023-02-20 19:41:06 -0800

    Minor changes

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,10 +5,6 @@ const Footer = () => {
   return (
     <div className="footer-container">
       <p>2023 DCOLLAN Headgear © All rights Reserved</p>
-      <p className="icons">
-        <AiFillInstagram />
-        <AiOutlineTwitter />
-      </p>
     </div>
   )
 }
